a3f6999706d8e3e4792f368a9643b71a1bb18166
bladevalor <karasirarichard250@gmail.com>
2025-10-27 19:20:56 +0200

    updated Entity class

--- src/Engine/Entity.hpp
+++ src/Engine/Entity.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#include "Engine/CommandQueue.hpp"
 #include "SceneNode.hpp"
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <cassert>
 
 class Entity : public SceneNode {
   public:
-    Entity(int hitpoints);
+    explicit Entity(int hitpoints) : mHintpoints(hitpoints) {}
 
   public:
     sf::Vector2f getVelocity() const { return mVelocity; }
@@ -24,19 +27,30 @@ class Entity : public SceneNode {
         mVelocity.y += vy;
     }
 
-  public:
-    void repair(int hitpoints);
-    void damage(int hitpoints);
-    void destroy();
-    void getHintpoints() const;
-    bool isDestroyed() const;
+    // virtual sf::FloatRect getBoundingBox() const;
 
-  private:
-    void updateCurrent(sf::Time dt) override {
+  public:
+    void repair(int hitpoints) {
+        assert(hitpoints > 0);
+
+        mHintpoints += hitpoints;
+    }
+
+    void damage(int hitpoints) {
+        assert(hitpoints > 0);
+        mHintpoints -= hitpoints;
+    }
+
+    void destroy() { mHintpoints = 0; }
+    int getHintpoints() const { return mHintpoints; }
+    bool isDestroyed() const { return mHintpoints <= 0; }
+
+  protected:
+    virtual void updateCurrent(sf::Time dt, CommandQueue &) {
         move(mVelocity * dt.asSeconds());
     }
 
   private:
     sf::Vector2f mVelocity;
-    int mHintPoints;
+    int mHintpoints;
 };
